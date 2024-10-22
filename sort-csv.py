2e2aa3a06228cf5907f5acdadb8ad3f30b4ace6b
Alan Akra <alanakra@hotmail.fr>
2024-10-22 16:11:28 +0200

    Generate file

--- sort-csv.py
+++ sort-csv.py
@@ -1,22 +1,29 @@
 import pandas as pd
+import pprint
+import datetime
 
 places = pd.read_csv('datas/data-sport-77.csv', sep= ';', usecols=['name', 'com_insee', 'com_nom'], encoding='utf-8')
 
 labelCount = 0
 
 placesList = set()
+current_time = datetime.datetime.now()
 
 for _, row in places.iterrows():
     name = row['name']
-    post = row['com_insee']
+    insee = row['com_insee']
     commune = row['com_nom']
     if not pd.isna(name):
-        placesList.add((name, post, commune))
+        placesList.add((name, insee, commune))
         labelCount += 1
         print('---')
         print(name)
-        print(post)
+        print(insee)
         print(commune)
-        print(placesList)
 
+with open (f'extracts/extract-77.py', 'w', encoding="utf-8") as extracted:
+    extracted.write(f'# Generated at {current_time} \n')
+    extracted.write(pprint.pformat(placesList))
+
+print('---')
 print(f'{labelCount} labeled places out of {len(places)} rows found.')
